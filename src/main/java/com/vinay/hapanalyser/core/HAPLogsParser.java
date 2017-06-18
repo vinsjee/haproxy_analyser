d4d984ee8a76eab4a724bb6559974df15598ded4
vinayagrawal <vinay.agrawal@myntra.com>
2017-06-18 22:51:21 +0530

    added multithreading-support

--- src/main/java/com/vinay/hapanalyser/core/HAPLogsParser.java
+++ src/main/java/com/vinay/hapanalyser/core/HAPLogsParser.java
@@ -4,24 +4,32 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.RecursiveAction;
 
-public class HAPLogsParser {
+public class HAPLogsParser extends RecursiveAction {
 
-	private String[] filesToBeAnalysed;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private LinkedList<String> filesToBeAnalysed;
 	private String urlToBeAnalsyed;
 	private String expectedResponseTime;
+	private ConcurrentHashMap<String, ServerParsedData> serverToServerDataMap;
 
-	public HAPLogsParser(String[] filesToBeAnalysed, String urlToBeAnalsyed, String expectedResponseTime) {
-		this.filesToBeAnalysed = filesToBeAnalysed;
+	public HAPLogsParser(LinkedList<String> listOfFilesToBeAnalysed, String urlToBeAnalsyed,
+			String expectedResponseTime, ConcurrentHashMap<String, ServerParsedData> serverToServerDataMap) {
+		this.filesToBeAnalysed = listOfFilesToBeAnalysed;
 		this.urlToBeAnalsyed = urlToBeAnalsyed;
 		this.expectedResponseTime = expectedResponseTime;
+		this.serverToServerDataMap=serverToServerDataMap;
 	}
 
-	public String[] getFileToBeAnalysed() {
+	public LinkedList<String> getFilesToBeAnalysed() {
 		return filesToBeAnalysed;
 	}
 
@@ -33,92 +41,183 @@ public class HAPLogsParser {
 		return expectedResponseTime;
 	}
 
-	public void startProcessingAndPrintData() throws IOException {
-		HashMap<String, ServerParsedData> serverToServerDataMap = new HashMap<String, ServerParsedData>();
-		System.out.println(ServerParsedData.headings());
-		for (String fileName : filesToBeAnalysed) {
-			BufferedReader fileReader = new BufferedReader(new FileReader(new File(fileName)));
-			try {
-				String hapLog;
-				while ((hapLog = fileReader.readLine()) != null) {
-					String[] hapSplittedLog = hapLog.split(" ");
+	// public void startProcessingAndPrintData() throws IOException {
+	// System.out.println(ServerParsedData.headings());
+	// for (String fileName : filesToBeAnalysed) {
+	// BufferedReader fileReader = new BufferedReader(new FileReader(new
+	// File(fileName)));
+	// try {
+	// String hapLog;
+	// while ((hapLog = fileReader.readLine()) != null) {
+	// String[] hapSplittedLog = hapLog.split(" ");
+	//
+	// if (hapSplittedLog.length < 23) {
+	// continue;
+	// }
+	// if (!hapSplittedLog[21].contains(getUrlToBeAnalsyed())) {
+	// continue;
+	// }
+	//
+	// String serverName = hapSplittedLog[9];
+	// String[] responseTimeData = hapSplittedLog[10].split("/");
+	// String requestDate = new
+	// StringBuilder(hapSplittedLog[2]).append(hapSplittedLog[1]).append("-")
+	// .append(hapSplittedLog[3]).toString();
+	// String httpMethodName = hapSplittedLog[20];
+	//
+	// ServerParsedData serverParsedData;
+	// if (serverToServedDataMap.containsKey(serverName)) {
+	// serverParsedData = serverToServedDataMap.get(serverName);
+	// } else {
+	// serverParsedData = new ServerParsedData();
+	// serverParsedData.setServerName(serverName);
+	// serverToServedDataMap.put(serverName, serverParsedData);
+	// }
+	// serverParsedData.setTotalRequests(serverParsedData.getTotalRequests() +
+	// 1);
+	// if (Long.valueOf(responseTimeData[4]) <=
+	// Long.valueOf(getExpectedResponseTime())) {
+	// serverParsedData.setTotRequestsWitinExpectedTime(
+	// serverParsedData.getTotRequestsWitinExpectedTime() + 1);
+	// }
+	// serverParsedData.getDates().add(requestDate);
+	// serverParsedData.getTimeTakenFromClientToHAP().add(Long.valueOf(responseTimeData[0]));
+	// serverParsedData.getTimeTakenInHAPQueues().add(Long.valueOf(responseTimeData[1]));
+	// serverParsedData.getTimeTakenForConnectionToServer().add(Long.valueOf(responseTimeData[2]));
+	// serverParsedData.getTimeTakenFromServerToHAP().add(Long.valueOf(responseTimeData[3]));
+	// serverParsedData.getTotalResponseTime().add(Long.valueOf(responseTimeData[4]));
+	// if (httpMethodName.contains("PUT")) {
+	// serverParsedData.setTotPUT(serverParsedData.getTotPUT() + 1);
+	// }
+	// if (httpMethodName.contains("GET")) {
+	// serverParsedData.setTotGET(serverParsedData.getTotGET() + 1);
+	// }
+	// if (httpMethodName.contains("POST")) {
+	// serverParsedData.setTotPOST(serverParsedData.getTotPOST() + 1);
+	// }
+	// if (httpMethodName.contains("DELETE")) {
+	// serverParsedData.setTotDELETE(serverParsedData.getTotDELETE() + 1);
+	// }
+	// }
+	//
+	// } finally {
+	// fileReader.close();
+	// }
+	// }
+	//
+	// List<ServerParsedData> values = new
+	// ArrayList<ServerParsedData>(serverToServedDataMap.values());
+	// Collections.sort(values);
+	// for (ServerParsedData serverParsedData : values) {
+	// serverParsedData.setP95ClientToHAP(calculate95Percentile(serverParsedData.getTimeTakenFromClientToHAP()));
+	// serverParsedData.setP95HAPQueue((calculate95Percentile(serverParsedData.getTimeTakenInHAPQueues())));
+	// serverParsedData
+	// .setP95HAPToServer(calculate95Percentile(serverParsedData.getTimeTakenForConnectionToServer()));
+	// serverParsedData
+	// .setP95ServerResponse(calculate95Percentile(serverParsedData.getTimeTakenFromServerToHAP()));
+	// serverParsedData.setP95TotResponse(calculate95Percentile(serverParsedData.getTotalResponseTime()));
+	// List<String> dates = serverParsedData.getDates();
+	// Collections.sort(dates);
+	// serverParsedData.setStartTime(dates.get(0));
+	// serverParsedData.setEndTime(dates.get(dates.size() - 1));
+	// System.out.println(serverParsedData);
+	// }
+	//
+	// }
 
-					if (hapSplittedLog.length < 23) {
-						continue;
-					}
-					if (!hapSplittedLog[21].contains(getUrlToBeAnalsyed())) {
-						continue;
-					}
+	// private long calculate95Percentile(List<Long> timeTakenList) {
+	// Collections.sort(timeTakenList);
+	// int size = timeTakenList.size();
+	// int index = (int) (size * 0.95);
+	// timeTakenList.get(index);
+	// return timeTakenList.get(index);
+	// }
 
-					String serverName = hapSplittedLog[9];
-					String[] responseTimeData = hapSplittedLog[10].split("/");
-					String requestDate = new StringBuilder(hapSplittedLog[2]).append(hapSplittedLog[1]).append("-")
-							.append(hapSplittedLog[3]).toString();
-					String httpMethodName = hapSplittedLog[20];
+	@Override
+	protected void compute() {
 
-					ServerParsedData serverParsedData;
-					if (serverToServerDataMap.containsKey(serverName)) {
-						serverParsedData = serverToServerDataMap.get(serverName);
-					} else {
-						serverParsedData = new ServerParsedData();
-						serverParsedData.setServerName(serverName);
-						serverToServerDataMap.put(serverName, serverParsedData);
-					}
-					serverParsedData.setTotalRequests(serverParsedData.getTotalRequests() + 1);
-					if (Long.valueOf(responseTimeData[4]) <= Long.valueOf(getExpectedResponseTime())) {
-						serverParsedData.setTotRequestsWitinExpectedTime(
-								serverParsedData.getTotRequestsWitinExpectedTime() + 1);
-					}
-					serverParsedData.getDates().add(requestDate);
-					serverParsedData.getTimeTakenFromClientToHAP().add(Long.valueOf(responseTimeData[0]));
-					serverParsedData.getTimeTakenInHAPQueues().add(Long.valueOf(responseTimeData[1]));
-					serverParsedData.getTimeTakenForConnectionToServer().add(Long.valueOf(responseTimeData[2]));
-					serverParsedData.getTimeTakenFromServerToHAP().add(Long.valueOf(responseTimeData[3]));
-					serverParsedData.getTotalResponseTime().add(Long.valueOf(responseTimeData[4]));
-					if (httpMethodName.contains("PUT")) {
-						serverParsedData.setTotPUT(serverParsedData.getTotPUT() + 1);
-					}
-					if (httpMethodName.contains("GET")) {
-						serverParsedData.setTotGET(serverParsedData.getTotGET() + 1);
-					}
-					if (httpMethodName.contains("POST")) {
-						serverParsedData.setTotPOST(serverParsedData.getTotPOST() + 1);
-					}
-					if (httpMethodName.contains("DELETE")) {
-						serverParsedData.setTotDELETE(serverParsedData.getTotDELETE() + 1);
-					}
-				}
-
-			} finally {
-				fileReader.close();
-			}
+		String fileToBeAnalysed = getFilesToBeAnalysed().poll();
+		if (getFilesToBeAnalysed().size() >= 1) {
+			HAPLogsParser hapLogsParser = new HAPLogsParser(getFilesToBeAnalysed(), getUrlToBeAnalsyed(),
+					getExpectedResponseTime(),getServerToServedDataMap());
+			hapLogsParser.fork();
+			//hapLogsParser.join();
 		}
-
-		List<ServerParsedData> values =  new ArrayList<ServerParsedData>(serverToServerDataMap.values());
-		Collections.sort(values);
-		for (ServerParsedData serverParsedData : values) {
-			serverParsedData.setP95ClientToHAP(calculate95Percentile(serverParsedData.getTimeTakenFromClientToHAP()));
-			serverParsedData.setP95HAPQueue((calculate95Percentile(serverParsedData.getTimeTakenInHAPQueues())));
-			serverParsedData
-					.setP95HAPToServer(calculate95Percentile(serverParsedData.getTimeTakenForConnectionToServer()));
-			serverParsedData
-					.setP95ServerResponse(calculate95Percentile(serverParsedData.getTimeTakenFromServerToHAP()));
-			serverParsedData.setP95TotResponse(calculate95Percentile(serverParsedData.getTotalResponseTime()));
-			List<String> dates = serverParsedData.getDates();
-			Collections.sort(dates);
-			serverParsedData.setStartTime(dates.get(0));
-			serverParsedData.setEndTime(dates.get(dates.size()-1));
-			System.out.println(serverParsedData);
+		try {
+			startProcessingData(fileToBeAnalysed);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}
 
-	private long calculate95Percentile(List<Long> timeTakenList) {
-		Collections.sort(timeTakenList);
-		int size = timeTakenList.size();
-		int index = (int) (size * 0.95);
-		timeTakenList.get(index);
-		return timeTakenList.get(index);
+	public ConcurrentHashMap<String, ServerParsedData> getServerToServerDataMap() {
+		return serverToServerDataMap;
+	}
+
+	private void startProcessingData(String fileToBeAnalysed) throws NumberFormatException, IOException {
+		BufferedReader fileReader = new BufferedReader(new FileReader(new File(fileToBeAnalysed)));
+		try {
+			String hapLog;
+			while ((hapLog = fileReader.readLine()) != null) {
+				String[] hapSplittedLog = hapLog.split(" ");
+
+				if (hapSplittedLog.length < 23) {
+					continue;
+				}
+				if (!hapSplittedLog[21].contains(getUrlToBeAnalsyed())) {
+					continue;
+				}
+
+				String serverName = hapSplittedLog[9];
+				String[] responseTimeData = hapSplittedLog[10].split("/");
+				String requestDate = new StringBuilder(hapSplittedLog[2]).append(hapSplittedLog[1]).append("-")
+						.append(hapSplittedLog[3]).toString();
+				String httpMethodName = hapSplittedLog[20];
+
+				ServerParsedData serverParsedData;
+				if (serverToServerDataMap.containsKey(serverName)) {
+					serverParsedData = serverToServerDataMap.get(serverName);
+				} else {
+					serverParsedData = new ServerParsedData();
+					serverParsedData.setServerName(serverName);
+					serverToServerDataMap.put(serverName, serverParsedData);
+				}
+				serverParsedData.setTotalRequests(serverParsedData.getTotalRequests() + 1);
+				if (Long.valueOf(responseTimeData[4]) <= Long.valueOf(getExpectedResponseTime())) {
+					serverParsedData
+							.setTotRequestsWitinExpectedTime(serverParsedData.getTotRequestsWitinExpectedTime() + 1);
+				}
+				serverParsedData.getDates().add(requestDate);
+				serverParsedData.getTimeTakenFromClientToHAP().add(Long.valueOf(responseTimeData[0]));
+				serverParsedData.getTimeTakenInHAPQueues().add(Long.valueOf(responseTimeData[1]));
+				serverParsedData.getTimeTakenForConnectionToServer().add(Long.valueOf(responseTimeData[2]));
+				serverParsedData.getTimeTakenFromServerToHAP().add(Long.valueOf(responseTimeData[3]));
+				serverParsedData.getTotalResponseTime().add(Long.valueOf(responseTimeData[4]));
+				if (httpMethodName.contains("PUT")) {
+					serverParsedData.setTotPUT(serverParsedData.getTotPUT() + 1);
+				}
+				if (httpMethodName.contains("GET")) {
+					serverParsedData.setTotGET(serverParsedData.getTotGET() + 1);
+				}
+				if (httpMethodName.contains("POST")) {
+					serverParsedData.setTotPOST(serverParsedData.getTotPOST() + 1);
+				}
+				if (httpMethodName.contains("DELETE")) {
+					serverParsedData.setTotDELETE(serverParsedData.getTotDELETE() + 1);
+				}
+			}
+
+		} finally {
+			fileReader.close();
+		}
+
+	}
+
+	public ConcurrentHashMap<String, ServerParsedData> getServerToServedDataMap() {
+		return this.serverToServerDataMap;
 	}
 
 }
--- src/main/java/com/vinay/hapanalyser/main/HAPLogAnalyser.java
+++ src/main/java/com/vinay/hapanalyser/main/HAPLogAnalyser.java
@@ -1,29 +1,74 @@
 package com.vinay.hapanalyser.main;
 
+import static java.util.Arrays.asList;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ForkJoinPool;
 
 import com.vinay.hapanalyser.core.HAPLogsParser;
+import com.vinay.hapanalyser.core.ServerParsedData;
 
 public class HAPLogAnalyser {
-	
-	
-	
+
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-	    try{ 
-	     System.out.println("File(s) to be analysed-In case of multiple file please provide the comma separated file name");
-	     String[] filesToBeAnalysed = sc.next().split(",");
-	     System.out.println("URL to be analysed");
-	     String urlToBeAnalsyed = sc.next();
-	     System.out.println("Expected response time - This will provide the results of how many request were withing this reponse time");
-	     String expectedResponseTime = sc.next();
-	     HAPLogsParser hapLogsParser = new HAPLogsParser(filesToBeAnalysed, urlToBeAnalsyed, expectedResponseTime);
-	     hapLogsParser.startProcessingAndPrintData();
-	    }finally{
-	    	sc.close();
-	    }
-	
+		try {
+			System.out.println(
+					"File(s) to be analysed-In case of multiple file please provide the comma separated file name");
+			String[] filesToBeAnalysed = sc.next().split(",");
+			LinkedList<String> listOfFilesToBeAnalysed = new LinkedList<String>(asList(filesToBeAnalysed));
+			System.out.println("URL to be analysed");
+			String urlToBeAnalsyed = sc.next();
+			System.out.println(
+					"Expected response time - This will provide the results of how many request were withing this reponse time");
+			String expectedResponseTime = sc.next();
+			ForkJoinPool forkJoinPool = new ForkJoinPool();
+			HAPLogsParser hapLogsParser = new HAPLogsParser(listOfFilesToBeAnalysed, urlToBeAnalsyed,
+					expectedResponseTime, new ConcurrentHashMap<String,ServerParsedData>());
+			forkJoinPool.invoke(hapLogsParser);
+			printData(hapLogsParser);
+			forkJoinPool.shutdown();
+		} finally {
+			sc.close();
+		}
+
+	}
+
+	private static void printData(HAPLogsParser hapLogsParser) {
+		ConcurrentHashMap<String, ServerParsedData> serverToServedDataMap = hapLogsParser.getServerToServedDataMap();
+		List<ServerParsedData> values = new ArrayList<ServerParsedData>(serverToServedDataMap.values());
+		Collections.sort(values);
+		System.out.println(ServerParsedData.headings());
+		for (ServerParsedData serverParsedData : values) {
+			serverParsedData.setP95ClientToHAP(calculate95Percentile(serverParsedData.getTimeTakenFromClientToHAP()));
+			serverParsedData.setP95HAPQueue((calculate95Percentile(serverParsedData.getTimeTakenInHAPQueues())));
+			serverParsedData
+					.setP95HAPToServer(calculate95Percentile(serverParsedData.getTimeTakenForConnectionToServer()));
+			serverParsedData
+					.setP95ServerResponse(calculate95Percentile(serverParsedData.getTimeTakenFromServerToHAP()));
+			serverParsedData.setP95TotResponse(calculate95Percentile(serverParsedData.getTotalResponseTime()));
+			List<String> dates = serverParsedData.getDates();
+			Collections.sort(dates);
+			serverParsedData.setStartTime(dates.get(0));
+			serverParsedData.setEndTime(dates.get(dates.size() - 1));
+			System.out.println(serverParsedData);
+
+		}
+
+	}
+
+	private static long calculate95Percentile(List<Long> timeTakenList) {
+		Collections.sort(timeTakenList);
+		int size = timeTakenList.size();
+		int index = (int) (size * 0.95);
+		timeTakenList.get(index);
+		return timeTakenList.get(index);
 	}
 
 }
